e9a352317ebf1b67630c9e7ff2e7ef506ed2f7b8
Night Coder <ksreekanthkr@gmail.com>
2020-04-03 02:27:48 +0530

    final commit

--- app/src/main/java/com/nightcoder/darkmode/BaseActivity.java
+++ app/src/main/java/com/nightcoder/darkmode/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.nightcoder.darkmode;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.app.UiModeManager;
 import android.content.ActivityNotFoundException;
@@ -11,27 +12,49 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.VideoController;
+import com.google.android.gms.ads.VideoOptions;
+import com.google.android.gms.ads.formats.MediaView;
+import com.google.android.gms.ads.formats.NativeAdOptions;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAdView;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class BaseActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
+    private static final String ADMOB_AD_UNIT_ID = "ca-app-pub-3940256099942544/2247696110";
     private RadioGroup radioGroup;
     private UiModeManager uiModeManager;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private InterstitialAd interstitialAd;
+
+    private UnifiedNativeAd nativeAd;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,6 +63,15 @@ public class BaseActivity extends AppCompatActivity implements RadioGroup.OnChec
 
         uiModeManager = ContextCompat.getSystemService(this, UiModeManager.class);
         radioGroup = findViewById(R.id.radioGroup);
+        RelativeLayout container = findViewById(R.id.container);
+        container.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        container.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         if (savedInstanceState == null) updateRadioGroup();
         radioGroup.setOnCheckedChangeListener(this);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -51,25 +83,194 @@ public class BaseActivity extends AppCompatActivity implements RadioGroup.OnChec
             findViewById(R.id.warn_disable).setVisibility(View.GONE);
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            new AlertDialog.Builder(this)
-                    .setIcon(R.drawable.ic_warning_black_24dp)
-                    .setTitle(R.string.alert_title)
-                    .setMessage(R.string.msg_android_version_compatible)
-                    .setCancelable(false)
-                    .setNegativeButton(getString(R.string.close), ((dialog, which) -> super.onBackPressed()))
-                    .setPositiveButton(R.string.uninstall, (dialog, which) -> {
-                        Intent i = new Intent();
-                        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                        i.setData(Uri.fromParts("package",
-                                BaseActivity.this.getPackageName(), null));
-                        startActivity(i);
-                        BaseActivity.this.finish();
-                    })
-                    .show();
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            new AlertDialog.Builder(this)
+//                    .setIcon(R.drawable.ic_warning_black_24dp)
+//                    .setTitle(R.string.alert_title)
+//                    .setMessage(R.string.msg_android_version_compatible)
+//                    .setCancelable(false)
+//                    .setNegativeButton(getString(R.string.close), ((dialog, which) -> super.onBackPressed()))
+//                    .setPositiveButton(R.string.uninstall, (dialog, which) -> {
+//                        Intent i = new Intent();
+//                        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//                        i.setData(Uri.fromParts("package",
+//                                BaseActivity.this.getPackageName(), null));
+//                        startActivity(i);
+//                        BaseActivity.this.finish();
+//                    })
+//                    .show();
+//        }
 
         sharedPreferences = getSharedPreferences("UserInfo", MODE_PRIVATE);
+        loadAds();
+        refreshAd();
+    }
+
+    private void loadAds() {
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getResources().getString(R.string.interstitial));
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+    }
+
+    private void loadInterstitial() {
+        if (interstitialAd.isLoaded()) {
+            interstitialAd.show();
+        } else if (!interstitialAd.isLoading()) {
+            int random = ThreadLocalRandom.current().nextInt(0, 3);
+            Log.d("Random", random + " number");
+            if (random == 1) {
+                interstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void populateUnifiedNativeAdView(UnifiedNativeAd nativeAd, UnifiedNativeAdView adView) {
+        // Set the media view. Media content will be automatically populated in the media view once
+        // adView.setNativeAd() is called.
+        MediaView mediaView = adView.findViewById(R.id.ad_media);
+        adView.setMediaView(mediaView);
+
+        // Set other ad assets.
+        adView.setHeadlineView(adView.findViewById(R.id.ad_headline));
+        adView.setBodyView(adView.findViewById(R.id.ad_body));
+        adView.setCallToActionView(adView.findViewById(R.id.ad_call_to_action));
+        adView.setIconView(adView.findViewById(R.id.ad_app_icon));
+        adView.setPriceView(adView.findViewById(R.id.ad_price));
+        adView.setStarRatingView(adView.findViewById(R.id.ad_stars));
+        adView.setStoreView(adView.findViewById(R.id.ad_store));
+        adView.setAdvertiserView(adView.findViewById(R.id.ad_advertiser));
+
+        // The headline is guaranteed to be in every UnifiedNativeAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeAd.getHeadline());
+
+        // These assets aren't guaranteed to be in every UnifiedNativeAd, so it's important to
+        // check before trying to display them.
+        if (nativeAd.getBody() == null) {
+            adView.getBodyView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getBodyView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getBodyView()).setText(nativeAd.getBody());
+        }
+
+        if (nativeAd.getCallToAction() == null) {
+            adView.getCallToActionView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getCallToActionView().setVisibility(View.VISIBLE);
+            ((Button) adView.getCallToActionView()).setText(nativeAd.getCallToAction());
+        }
+
+        if (nativeAd.getIcon() == null) {
+            adView.getIconView().setVisibility(View.GONE);
+        } else {
+            ((ImageView) adView.getIconView()).setImageDrawable(
+                    nativeAd.getIcon().getDrawable());
+            adView.getIconView().setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getPrice() == null) {
+            adView.getPriceView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getPriceView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getPriceView()).setText(nativeAd.getPrice());
+        }
+
+        if (nativeAd.getStore() == null) {
+            adView.getStoreView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getStoreView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getStoreView()).setText(nativeAd.getStore());
+        }
+
+        if (nativeAd.getStarRating() == null) {
+            adView.getStarRatingView().setVisibility(View.INVISIBLE);
+        } else {
+            ((RatingBar) adView.getStarRatingView())
+                    .setRating(nativeAd.getStarRating().floatValue());
+            adView.getStarRatingView().setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getAdvertiser() == null) {
+            adView.getAdvertiserView().setVisibility(View.INVISIBLE);
+        } else {
+            ((TextView) adView.getAdvertiserView()).setText(nativeAd.getAdvertiser());
+            adView.getAdvertiserView().setVisibility(View.VISIBLE);
+        }
+
+        // This method tells the Google Mobile Ads SDK that you have finished populating your
+        // native ad view with this native ad. The SDK will populate the adView's MediaView
+        // with the media content from this native ad.
+        adView.setNativeAd(nativeAd);
+
+        // Get the video controller for the ad. One will always be provided, even if the ad doesn't
+        // have a video asset.
+        VideoController vc = nativeAd.getVideoController();
+
+        // Updates the UI to say whether or not this ad has a video asset.
+        if (vc.hasVideoContent()) {
+            // Create a new VideoLifecycleCallbacks object and pass it to the VideoController. The
+            // VideoController will call methods on this object when events occur in the video
+            // lifecycle.
+            vc.setVideoLifecycleCallbacks(new VideoController.VideoLifecycleCallbacks() {
+                @Override
+                public void onVideoEnd() {
+                    // Publishers should allow native ads to complete video playback before
+                    // refreshing or replacing them with another ad in the same UI location.
+                    super.onVideoEnd();
+                }
+            });
+        }
+    }
+
+    private void refreshAd() {
+        AdLoader.Builder builder = new AdLoader.Builder(this, ADMOB_AD_UNIT_ID);
+
+        // OnUnifiedNativeAdLoadedListener implementation.
+        builder.forUnifiedNativeAd(unifiedNativeAd -> {
+            // You must call destroy on old ads when you are done with them,
+            // otherwise you will have a memory leak.
+            if (nativeAd != null) {
+                nativeAd.destroy();
+            }
+            nativeAd = unifiedNativeAd;
+            FrameLayout frameLayout =
+                    findViewById(R.id.fl_adplaceholder);
+            @SuppressLint("InflateParams")
+            UnifiedNativeAdView adView = (UnifiedNativeAdView) getLayoutInflater()
+                    .inflate(R.layout.ad_unified, null);
+            populateUnifiedNativeAdView(unifiedNativeAd, adView);
+            frameLayout.removeAllViews();
+            frameLayout.addView(adView);
+        });
+
+        VideoOptions videoOptions = new VideoOptions.Builder()
+                .setStartMuted(true)
+                .build();
+
+        NativeAdOptions adOptions = new NativeAdOptions.Builder()
+                .setVideoOptions(videoOptions)
+                .build();
+
+        builder.withNativeAdOptions(adOptions);
+
+        AdLoader adLoader = builder.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Toast.makeText(BaseActivity.this, "Failed to load native ad: "
+                        + errorCode, Toast.LENGTH_SHORT).show();
+            }
+        }).build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (nativeAd != null) {
+            nativeAd.destroy();
+        }
+        super.onDestroy();
     }
 
     private void updateRadioGroup() {
@@ -106,31 +307,39 @@ public class BaseActivity extends AppCompatActivity implements RadioGroup.OnChec
             window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
             Button later = dialog.findViewById(R.id.button_later);
             Button rate = dialog.findViewById(R.id.rate_button);
+            RatingBar ratingBar = dialog.findViewById(R.id.rating);
             later.setOnClickListener(v -> super.onBackPressed());
             rate.setOnClickListener(v -> {
-                Uri uri = Uri.parse("market://details?id=" + getPackageName());
-                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
-                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
-                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
-                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-                try {
-                    startActivity(goToMarket);
-                    editor = sharedPreferences.edit();
-                    editor.putBoolean("rate", true);
-                    editor.apply();
-                    dialog.cancel();
-                } catch (ActivityNotFoundException e) {
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
-                    editor = sharedPreferences.edit();
-                    editor.putBoolean("rate", true);
-                    editor.apply();
-                    dialog.cancel();
-                }
+                rating();
+                dialog.cancel();
             });
+            ratingBar.setOnClickListener(v -> {
+                rating();
+                dialog.cancel();
+            });
+
             dialog.show();
         }
+    }
 
+    private void rating() {
+        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            startActivity(goToMarket);
+            editor = sharedPreferences.edit();
+            editor.putBoolean("rate", true);
+            editor.apply();
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+            editor = sharedPreferences.edit();
+            editor.putBoolean("rate", true);
+            editor.apply();
+        }
     }
 
     @Override
@@ -140,16 +349,19 @@ public class BaseActivity extends AppCompatActivity implements RadioGroup.OnChec
                 uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_NO);
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
                     uiModeManager.disableCarMode(0);
+                loadInterstitial();
                 break;
             case R.id.radioNight:
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
                     uiModeManager.enableCarMode(0);
                 uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
+                loadInterstitial();
                 break;
             case R.id.radioAuto:
                 uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_AUTO);
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
                     uiModeManager.disableCarMode(0);
+                loadInterstitial();
                 break;
         }
     }
